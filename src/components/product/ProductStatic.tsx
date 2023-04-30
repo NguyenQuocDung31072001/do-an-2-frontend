7ca3e00d7405ddfc75d161c3af58701c89832b23
dung.nq2 <dungnguyen310701@gmail.com>
2023-04-30 10:27:51 +0700

    fix ui slider

--- src/components/product/ProductStatic.tsx
+++ src/components/product/ProductStatic.tsx
@@ -10,7 +10,7 @@ interface IProductStaticComponent {
 export default function ProductStatic({ product }: IProductStaticComponent) {
   const navigate = useNavigate()
   return (
-    <div className='flex cursor-pointer flex-col' onClick={() => navigate(PathRouter.PRODUCT.PEODUCT_DETAIL)}>
+    <div className='mx-2 flex cursor-pointer flex-col' onClick={() => navigate(PathRouter.PRODUCT.PEODUCT_DETAIL)}>
       <img src={product.image} alt='' />
       <p className='truncate'>{product.name}</p>
       <p>{product.price}</p>
